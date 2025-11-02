14c289848444feae747e87e948c47e727eed53fe
Евгений Сыроежкин <evgeniy.resuser@icloud.com>
2025-11-02 17:20:18 +0700

    Added logging to 2.1, added 2.2

--- src/clojure_labs/integral.clj
+++ src/clojure_labs/integral.clj
@@ -3,45 +3,45 @@
 (defn trapecii [f h]                         ;; f - f(t), h - step
   (let [cache (atom {0 0.0})]                              ;; atom with map x -> val | upper-boundary => I(from 0 to upper-boundary)
     (fn [t]                                                 ;; returns F(t)
-      (let [m @cache                                        ;; get map from cahce
-          [nearest-t nearest-F] (apply min-key (fn [entry] (abs (- (key entry) t))) m) ;; (min-key fn col) returns an arg x where fn(x) is min
-          dir (if (>= t nearest-t) 1 -1)
-          step (* dir h)
-          dist (abs (- t nearest-t))
-          steps (int (/ dist h))
-          remain (- dist (* steps h))                     ;; what can not be covered with h
-          acc
-          (loop [i 0
-                 sum 0.0
-                 prev-y (f nearest-t)
-                 x nearest-t
-                 ]
-            (if (< i steps)
-              (let [x-next (+ x step)                     ;; have some int steps
-                    y (f x-next)
-                    inc-val (* 0.5 h (+ prev-y y))]       ;; trap S
-                (swap! cache (fn [old-m] (assoc old-m x-next (+ nearest-F sum inc-val)))) ;; save next arg and value
-                (recur (inc i) (+ sum inc-val) y x-next))
-              (if (pos? remain)                           ;; no int steps left -> need to add some or not
-                (let [x-before (+ nearest-t (* dir steps h))
-                      inc-last (* 0.5 remain (+ (f x-before) (f t)))
-                      ]
-                  (+ sum inc-last)
+      (let [m @cache                                        ;; get map from cache
+            [nearest-t nearest-F] (apply min-key (fn [entry] (abs (- (key entry) t))) m)] ;; find nearest cached point
+        (println "calculating F(" t "), nearest-t:" nearest-t ", F(nearest-t):" nearest-F "cache size" (count m))
+        (let [dir (if (>= t nearest-t) 1 -1)
+              step (* dir h)
+              dist (abs (- t nearest-t))
+              steps (int (/ dist h))
+              remain (- dist (* steps h))
+              acc
+              (loop [i 0
+                     sum 0.0
+                     prev-y (f nearest-t)
+                     x nearest-t]
+                (if (< i steps)
+                  (let [x-next (+ x step)                     ;; have some int steps
+                        y (f x-next)
+                        inc-val (* 0.5 h (+ prev-y y))]       ;; trap S
+                    (swap! cache (fn [old-m] (assoc old-m x-next (+ nearest-F sum inc-val)))) ;; save next arg and value
+                    (println "putting" x-next (+ nearest-F sum inc-val))
+                    (recur (inc i) (+ sum inc-val) y x-next))
+                  (if (pos? remain)                           ;; no int steps left -> need to add some or not
+                    (let [x-before (+ nearest-t (* dir steps h))
+                          inc-last (* 0.5 remain (+ (f x-before) (f t)))]
+                      (+ sum inc-last)
+                    )
+                    sum
+                  )
                 )
-                sum
               )
-            )
+            ]
+          (let [res (+ nearest-F acc)]                        ;; F(t) = F(nearest) + acc
+            (swap! cache (fn [old-m] (assoc old-m t res)))
+            res
           )
-        ]
-        (let [res (+ nearest-F acc)]                        ;; F(t) = F(nearest) + acc
-          (swap! cache (fn [old-m] (assoc old-m t res)))
-          res
         )
       )
     )
   )
 )
-
 
 (defn integral [f h]
   (let [F (trapecii f h)]
@@ -51,9 +51,12 @@
   )
 )
 
-(let [f (fn [t] (* t t)) I (integral f 0.0000001)]
+;; пример
+(let [f (fn [t] (* t t))
+      I (integral f 0.001)]
   (println (I 1))
   (println (I 2))
-  (println (I 5))
-  (println (I 3))
+  ;(println (I 5))
+  ;(println (I 3))
 )
+
---
+++ src/clojure_labs/integral2.clj
@@ -0,0 +1,54 @@
+(ns clojure-labs.integral2)
+
+(defn make-primitive [f h]
+  (let [calls (atom 0)            ;; counter of calls for f
+        xf (fn [x]
+             (swap! calls inc)
+             (f x))
+        xs    (iterate #(+ % h) 0.0)                        ;; creating a lazy sequence 0, h, 2h ...
+        fvals (map xf xs)                                   ;; call f+counter on every xs value
+        pairs (map vector fvals (rest fvals))               ;; pairs of neighbours - [f(0),f(h)],[f(h),f(2h)]
+        acc   (reductions                                   ;; seq of accumulated areas - 0,S1,S2,S3
+                (fn [prev [a b]]
+                  (+ prev (* h (/ (+ a b) 2.0)))
+                )
+                0.0
+                pairs
+              )
+        ]
+    (fn [x]                                                 ;; return F(x) with closure
+      (let [x (double x)]
+        (let [steps (int (Math/floor (/ x h)))            ;; int steps
+              s   (nth acc steps)                           ;; trap S: 0 to n
+              fnk (nth fvals steps)                         ;; f(n)
+              fx  (xf x)                                    ;; f(x) + inc counter
+              r   (- x (* steps h))                         ;; difference
+              area (* r (/ (+ fnk fx) 2.0))]
+          (println "total calls:" @calls ", went to index" steps)
+          (+ s area)
+        )
+      )
+    )
+  )
+)
+
+
+(defn integral [f h]
+  (let [F (make-primitive f h)]
+    (fn [x]
+      (- (F x) (F 0))
+    )
+  )
+)
+
+;; пример
+(let [f (fn [t] (* t t))
+      I (integral f 0.001)
+      ]
+  (println (I 1))
+  (println (I 2))
+  (println (I 1.5))
+)
+
+;; несколько вызовов в разных точках — последовательность частичных сумм
+;; будет лениво наращиваться и переиспользоваться:
